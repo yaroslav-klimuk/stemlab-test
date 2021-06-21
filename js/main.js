adcacad4aa939078d282a0ce8eabbc4233ca1eca
yaroslavklimuk21 <yaroslavklimuk21@gmail.com>
2021-06-21 19:30:32 +0300

    feat: add modal

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 const requestURL = 'https://api.tachyon-analytics.com/predict/';
 const imgWrapper = document.getElementById('img-wrapper');
+const popup = document.querySelector('.popup');
+const mainBtn = document.querySelector('.main-btn');
+const form = document.getElementById('form');
+const formWrapper = document.querySelector('.form-wrapper');
+
+mainBtn.addEventListener('click', () => {
+	formWrapper.classList.toggle('__active');
+	popup.classList.toggle('__active');
+});
+popup.addEventListener('click', (e) => {
+	e.stopPropagation();
+	formWrapper.classList.remove('__active');
+	popup.classList.remove('__active');
+	imgWrapper.innerHTML = '';
+	form.reset();
+});
+formWrapper.addEventListener('click', (e) => {
+	e.stopPropagation();
+});
+
 function sendRequest(method, url, body) {
 	return fetch(url, { method, body }).then((response) => {
 		if (response.ok) {
 			return response.json().then((text) => {
-				imgWrapper.innerHTML = `<img src="https://api.tachyon-analytics.com/predict/${text.url}" alt="graph" id="imgDiv"></img>`;
+				formWrapper.classList.remove('__active');
+				imgWrapper.innerHTML = `<img src="https://api.tachyon-analytics.com/predict/${text.url}" alt="graph" id="imgDiv" onclick="event.stopPropagation()"></img>`;
 			});
 		}
 		return response.text().then((error) => {
@@ -15,7 +36,6 @@ function sendRequest(method, url, body) {
 	});
 }
 
-const form = document.getElementById('form');
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const formData = new FormData(form);
